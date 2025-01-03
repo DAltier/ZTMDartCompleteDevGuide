8088e32e3f06da6a437cd8a861b1403197680ee0
DAltier <altierdana@gmail.com>
2025-01-02 20:05:55 -0800

    class constructors

---
+++ RestaurantRatings/restaurant_ratings.dart
@@ -0,0 +1,40 @@
+class Restaurant {
+  const Restaurant({
+    required this.name,
+    required this.cuisine,
+    required this.ratings,
+  });
+  final String name;
+  final String cuisine;
+  final List<double> ratings;
+}
+
+void main() {
+  var restaurants = [
+    {
+      'name': 'Pizza Mario',
+      'cuisine': 'Italian',
+      'ratings': [5.0, 3.5, 4.5],
+    },
+    {
+      'name': 'Chez Anne',
+      'cuisine': 'French',
+      'ratings': [5.0, 4.5, 4.0],
+    },
+    {
+      'name': 'Navaratna',
+      'cuisine': 'Indian',
+      'ratings': [4.0, 4.5, 4.0],
+    },
+  ];
+  for (var restaurant in restaurants) {
+    final ratings = restaurant['ratings'] as List<double>;
+    var total = 0.0;
+    for (var rating in ratings) {
+      total += rating;
+    }
+    final avgRating = total / ratings.length;
+    restaurant['avgRating'] = avgRating;
+    print(restaurant);
+  }
+}
